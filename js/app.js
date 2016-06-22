57e913dc364a374e558a12971a79d80230bff01c
Andrew Wong <andrew.wongsf@gmail.com>
2016-06-22 16:35:31 -0400

    Linked jQuery and javascript file to index
    Via jQuery, gave focus to first text field when page loads

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* ===================
+  	  Javascript
+====================== */
+
+// When the page loads, give focus to the first text field
+$(document).ready(function () {
+	$("input:text")[0].focus();
+});
