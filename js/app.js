850c4232cd2cf4554b3ef6c900e8701beafcf180
Andrew Wong <andrew.wongsf@gmail.com>
2016-07-03 19:57:40 -0400

    clean up and commenting to document code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
-/* ===================
-  	  Javascript
-====================== */
-
-// When the page loads, give focus to the first text field
-// $(document).ready(function () {
-// 	$("input:text")[0].focus();
-
-// 	$("select").addClass("turnintodropdown");
-
-
-	
-// 	setTimeout(function() {
-// 		$("#payment ~ a").text("Credit Card");
-// 		$("#payment").val("credit card");
-//     }, 400);
-
-// });
+// ON PAGE LOAD
+// Give focus to the first text field, add class to style drop down menus, 
+// hide 'other' field, hide color label, hide paypal and bitcoin text, 
+// wrap drop down menus with div for styling, and set initial payment option 
+// to be Credit Card
 
 $("input:text")[0].focus();
 
 $("select").addClass("turnintodropdown");
-	
+
+$("form > fieldset:nth-child(1)").append("<input type='text' id='other-title' placeholder='Your Title'>");
+$("#other-title").hide();
+
+$("#colors-js-puns").hide();
+
+$("#credit-card").next().attr("id", "paypal");
+$("#paypal").next().attr("id", "bitcoin");
+
+$("#paypal").hide();
+$("#bitcoin").hide();
+
+$("#exp-month").wrap("<div id='expiry'></div>");
+$("#exp-year").wrap("<div id='expiry'></div>");
+$("#title").wrap("<div id='job'></div>");
+$("#other-title").wrap("<div id='job'></div>");
+
+// Wait for drop down styling to load
 setTimeout(function() {
 	$("#payment ~ a").text("Credit Card");
 	$("#payment").val("credit card");
 }, 400);
 
-/*
-Reveal a text field when the "Other" option is selected from the "Job Role" drop down menu
-*/
-$("form > fieldset:nth-child(1)").append("<input type='text' id='other-title' placeholder='Your Title'>");
-$("#other-title").hide();
+// JOB ROLE
+// Reveal text field when "Other" option is selected from the "Job Role" drop down menu
 
 $("body").on("click", "#title ~ .dropcontainer > ul > li:last-child", function (){
 	$("#other-title").show();
@@ -40,13 +41,12 @@ $("body").on("click", "#title ~ .dropcontainer > ul > li:not(:last-child)", func
 	$("#other-title").hide();
 });
 
-/*
-For the T-Shirt color menu, only display the options that match the design selected in the "Design" menu.
-If the user selects "Theme - JS Puns" 
-	then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-If the user selects "Theme - I ♥ JS" 
-	then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
-*/
+// T-SHIRT INFO
+// If the user selects "Theme - JS Puns" 
+// 	color menu only displays "Cornflower Blue," "Dark Slate Grey," and "Gold."
+// If the user selects "Theme - I ♥ JS" 
+// 	color menu only displays "Tomato," "Steel Blue," and "Dim Grey."
+
 
 $("body").on("click", "#design ~ .dropcontainer > ul > li:nth-child(1)", function () {
 	$("#colors-js-puns").hide();
@@ -68,17 +68,12 @@ $("body").on("click", "#design ~ .dropcontainer > ul > li:nth-child(3)", functio
 	$("#color ~ .dropcontainer > ul > li:nth-child(n+4").show();
 });
 
-/*
-Some events are at the same time as others. 
-If the user selects a workshop, don't allow selection of a workshop at the same date and 
-time -- you should disable the checkbox and visually indicate that the workshop in the 
-competing time slot isn't available.
-	When a user unchecks an activity, make sure that competing activities (if there are any)
-	are no longer disabled.
-As a user selects activities to register for, a running total is listed below the list of 
-checkboxes. For example, if the user selects "Main conference" then Total: $200 should appear. 
-If they add 1 workshop the total should change to Total: $300. 
-*/
+// ACTIVITIES
+// When user selects a workshop, turn workshop text in the competing time slot grey to show
+// it isn't available and disable checkbox.
+// When user unchecks activity, make competing timeslots active again.
+// Each time user selects activities to register for, list a running total below the list of 
+// checkboxes.
 
 var jsFrameworks = $("input[name='js-frameworks'");
 var jsLibraries = $("input[name='js-libs']");
@@ -172,25 +167,9 @@ $("input[name='npm']").change(function () {
 	}
 });
 
-/* Payment Info section of the form. 
 
-Display payment sections based on chosen payment option
-
-	The "Credit Card" payment option should be selected by default and result in the display of the
-	#credit-card div, and hide the "Paypal" and "Bitcoin information.
-	
-	When a user selects the "PayPal" payment option, display the Paypal information, and hide the 
-	credit card information and the "Bitcoin" information.
-	
-	When a user selects the "Bitcoin" payment option, display the Bitcoin information, and hide the 
-	credit card information.
-*/
-
-$("#credit-card").next().attr("id", "paypal");
-$("#paypal").next().attr("id", "bitcoin");
-
-$("#paypal").hide();
-$("#bitcoin").hide();
+// PAYMENT INFO
+// Display payment sections based on chosen payment option
 
 $("body").on("click", "#payment ~ .dropcontainer > ul > li:nth-child(1)", function (){
 
@@ -224,24 +203,14 @@ $("body").on("click", "#payment ~ .dropcontainer > ul > li:nth-child(4)", functi
 
 });
 
-/* Form validation. 
-
-Display error messages and don't let the user submit the form if any of these validation errors exist:
-	
-	Name field can't be empty
-	
-	Email field must be a validly formatted e-mail address (you don't have to check that it's a real 
-	e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example. 
-	You'll need to use a regular expression to get this requirement. See the list of Resources 
-	for links to learn about regular expressions.
-
-	At least one activity must be checked from the list under "Register for Actitivities."
-
-	Payment option must be selected.
-
-	If "Credit card" is the selected payment option, make sure the user supplied a credit card number, 
-	a zip code, and a 3 number CVV value.
-*/
+// FORM VALIDATION
+// Check for the following errors and display error message if neccessary:
+// 		Name field can't be empty
+// 		Email field must be validly formatted
+// 		At least one activity must be checked
+// 		A payment option must be selected.
+// 		If "Credit card" is selected, credit card must be valid,
+//		zip code and a 3 number CVV must be entered.
 
 var errorMsg = "";
 var emailInput = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
@@ -297,21 +266,5 @@ $("form").submit(function (event) {
 	$("#error").show();
 			
 });
-
-// Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
-
-$("#colors-js-puns").hide();
-
-// Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields 
-// (see Resources links for an article on how to improve the look of select menus using CSS and JavaScript).
-
-
-// Validate the credit card number so that it's a validly formatted credit card number. 
-// (see the Resources links for information on how to do this.)
-
-$("#exp-month").wrap("<div id='expiry'></div>");
-$("#exp-year").wrap("<div id='expiry'></div>");
-$("#title").wrap("<div id='job'></div>");
-$("#other-title").wrap("<div id='job'></div>");
 
 	
